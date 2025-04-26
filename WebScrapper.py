b0df9a78fc7db3f4b9fa588d1408c5d586f29e6b
LavakushDev <lavakush.learning@outlook.com>
2025-04-26 20:12:32 +0800

    Added MySQL Code

--- WebScrapper.py
+++ WebScrapper.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import re
+import mysql.connector
 
 base_url = "https://reiwa.com.au/for-sale/broadwood+karlkurla+binduli+hannans+kalgoorlie+boulder+fimiston+south-boulder+victory-heights/?includesurroundingsuburbs=true&sortby=default&page="
 headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
         }
 
-for page in range(1, 25):
+for page in range(1, 3):
     url = base_url + str(page)
     print(f"Scraping page {page}...")
 
@@ -21,21 +22,48 @@ for page in range(1, 25):
             
             if (property_cards == []):
                 break
+            
+            
 
             for card in property_cards:
                 href = card.get('href')
                 price_tag = card.find('p', class_='heading h4 sk-item')
+                
 
                 if href and price_tag:
                     full_link = 'https://reiwa.com.au' + href
                     raw_price = price_tag.get_text(strip=True)
                     price = re.sub(r'[^\d]', '', raw_price)
-
+                    
                     path = href.strip('/')
                     path = re.sub(r'-\d+$', '', path)
                     address = path.replace('-', ' ')
                     address = address.title()
-                    print(f"Address: {address}, Price: {price}, Link: {full_link}")
+                    address_id = full_link.rstrip('/').split('-')[-1]
+                    
+
+                    if(int(price) < 450000):
+
+                        print(f"Address: {address}, Price: {price}, Link: {full_link}")
+                        conn = mysql.connector.connect(
+                        host="192.168.1.108",  
+                        user="myuser",
+                        password="mypassword",
+                        database="Houses_Scrapper"
+                        )
+
+                        cursor = conn.cursor()
+
+                        sql = "INSERT INTO Houses_List (address, house_id, price, web_url) VALUES (%s, %s, %s, %s)"
+                        values = (address, address_id, price, full_link)
+
+                        cursor.execute(sql, values)
+                        conn.commit()
+
+                        print(cursor.rowcount, "record inserted.")
+
+                        cursor.close()
+                        conn.close()
 
         else:
             print(f"Failed to retrieve page {page}. Status code: {response.status_code}")
